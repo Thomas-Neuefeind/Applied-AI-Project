7a2971ad8ac24f5cf45c3f777db33ff277fd75da
Thomas-Neuefeind <tneuefei@vols.utk.edu>
2021-12-02 17:54:21 -0500

    first commit

---
+++ Alzeimer's Prediction.py
@@ -0,0 +1,28 @@
+from re import M
+import numpy as np
+from numpy.core.numeric import cross
+import pandas as pd
+import pylab as pl
+from sklearn import svm, datasets
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.svm import SVC
+
+
+cross_sectional = pd.read_csv("oasis_cross-sectional.csv")
+longitudinal = pd.read_csv("oasis_longitudinal.csv")
+
+
+#filter any rows with missing data
+cross_sectional.dropna(axis = 0, how = 'any', inplace=True)
+longitudinal.dropna(axis = 0, how = 'any', inplace=True)
+
+cross_sectional= cross_sectional.drop(columns=['ID', 'Hand', 'Delay'])
+longitudinal = longitudinal.drop(columns=['Subject ID', 'MRI ID', 'MR Delay', 'Visit', 'Hand'])
+longitudinal = longitudinal.rename(columns={'EDUC':'Educ'})
+
+data = pd.concat([cross_sectional, longitudinal])
+print(data)
